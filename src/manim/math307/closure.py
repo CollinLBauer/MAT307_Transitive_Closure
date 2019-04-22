ac0ce39b7f78fa90700bf64f3ccee30763685ec6
osboxes.org <collinlbauer@gmail.com>
2019-04-22 19:35:40 -0400

    added edge list

--- src/manim/math307/closure.py
+++ src/manim/math307/closure.py
@@ -22,6 +22,7 @@ class FromInput(Scene):
 
         vertStr = "" # stores vertices from file
         edgeStr = "" #stores edges from file
+
         #vertices and edges must all be on same line
         #vertices line must be labeled with 'v-'
         #edge line must be labeled with 'e-'
@@ -34,13 +35,12 @@ class FromInput(Scene):
                 if ln[0] == 'e' or ln[0] == 'E':
                     edgeStr = ln
 
-#        vertLbls = []       
-#        verts = []          
+        #loop; build vertLbls[] and verts[]
         vertStr = vertStr[2:len(vertStr)-1].split(";")
 #        print(vertStr) #debug
         for i in range(len(vertStr)):
             self.vertLbls.append(vertStr[i][0])
-#            print(vert) #debug
+#            print(vertStr[i]) #debug
             for j in range(0,44):
                 char = chr(j)
                 vertStr[i] = vertStr[i].replace(char,"")
@@ -51,17 +51,23 @@ class FromInput(Scene):
                 char = chr(j)
                 vertStr[i] = vertStr[i].replace(char,"")
             vertStr[i] = vertStr[i].split(",")
-#            print(vert) #debug
+#            print(vertStr[i]) #debug
             for j in range(len(vertStr[i])):
                 vertStr[i][j] = int(vertStr[i][j])
             self.verts.append(vertStr[i])
 
-        print(self.vertLbls)
-        print(self.verts)
-        
-        
-#        print("vertStr: \"" + vertStr + "\"")
-#        print("edgeStr: \"" + edgeStr + "\"")
+        #loop: build edges[]
+        edgeStr = edgeStr[2:len(edgeStr)-1].split(";")
+#        print(edgeStr) #debug
+        for i in range((len(edgeStr))):
+#            print(edgeStr[i]) #debug
+            edgeStr[i] = edgeStr[i].replace("(","")
+            edgeStr[i] = edgeStr[i].replace(")","")
+            edgeStr[i] = edgeStr[i].split(",")
+#            print(edgeStr[i]) #debug
+            self.edges.append(edgeStr[i])
+
+                
 
 
 
@@ -70,7 +76,8 @@ class FromInput(Scene):
         print("Please backup any needed animations before continuing.")
         self.buildGraph()
 
-        print(self.vertLbls)
-        print(self.verts)
+        print("Labels: " + str(self.vertLbls))
+        print("Vertices: " + str(self.verts))
+        print("Edges: " + str(self.edges))
 
         self.wait(3)
